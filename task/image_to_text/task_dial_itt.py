fd98c6430996a39c5dcb55cf5fc2879695364d9e
Roman Bezhan <roman_bezhan@epam.com>
2026-02-18 14:36:55 +0300

    added task_dial_itt.py

--- task/image_to_text/task_dial_itt.py
+++ task/image_to_text/task_dial_itt.py
@@ -14,31 +14,42 @@ async def _put_image() -> Attachment:
     file_name = 'dialx-banner.png'
     image_path = Path(__file__).parent.parent.parent / file_name
     mime_type_png = 'image/png'
-    # TODO:
-    #  1. Create DialBucketClient
-    #  2. Open image file
-    #  3. Use BytesIO to load bytes of image
-    #  4. Upload file with client
-    #  5. Return Attachment object with title (file name), url and type (mime type)
-    raise NotImplementedError
+    
+    async with DialBucketClient(api_key=API_KEY, endpoint=DIAL_URL) as bucket_client:
+        with open(image_path, 'rb') as image_file:
+            image_bytes = image_file.read()
+        
+        image_content = BytesIO(image_bytes)
+
+        attachment = await bucket_client.put_file(content=image_content, name=file_name, type=mime_type_png)
+
+        return Attachment(
+            title=file_name,
+            url=attachment.get("url"),
+            type=mime_type_png
+        )
 
 
 def start() -> None:
-    # TODO:
-    #  1. Create DialModelClient
-    #  2. Upload image (use `_put_image` method )
-    #  3. Print attachment to see result
-    #  4. Call chat completion via client with list containing one Message:
-    #    - role: Role.USER
-    #    - content: "What do you see on this picture?"
-    #    - custom_content: CustomContent(attachments=[attachment])
-    #  ---------------------------------------------------------------------------------------------------------------
-    #  Note: This approach uploads the image to DIAL bucket and references it via attachment. The key benefit of this
-    #        approach that we can use Models from different vendors (OpenAI, Google, Anthropic). The DIAL Core
-    #        adapts this attachment to Message content in appropriate format for Model.
-    #  TRY THIS APPROACH WITH DIFFERENT MODELS!
-    #  Optional: Try upload 2+ pictures for analysis
-    raise NotImplementedError
+    model_client = DialModelClient(
+        api_key=API_KEY,
+        endpoint=DIAL_CHAT_COMPLETIONS_ENDPOINT,
+        deployment_name="anthropic.claude-v3-haiku"
+    )
 
+    attachment = asyncio.run(_put_image())
+    print(attachment)
+
+    model_client.get_completion(
+        [
+            Message(
+                role=Role.USER,
+                content="What is on this image?",
+                custom_content=CustomContent(
+                    attachment=[attachment]     
+                )
+            )
+        ]
+    )
 
 start()
